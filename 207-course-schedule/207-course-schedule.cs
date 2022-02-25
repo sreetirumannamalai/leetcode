ebc615c5349f58f02069084da5392e6814e84a8f
sreetirumannamalai <sree.tirumannamalai@gmail.com>
2022-02-25 00:03:50 -0800

    Time: 104 ms (95.43%), Space: 44.6 MB (9.24%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.cs
@@ -0,0 +1,46 @@
+public class Solution {
+    public bool CanFinish(int numCourses, int[][] prerequisites) {
+        int[] visited = new int[numCourses];
+        //create an adjList
+        Dictionary<int, List<int>> adjList = new Dictionary<int, List<int>>();
+        for(int i=0;i<numCourses;i++)
+        {
+             adjList.Add(i, new List<int>());
+        }
+        
+        for(int i=0;i<prerequisites.Length;i++)
+        {
+            adjList[prerequisites[i][0]].Add(prerequisites[i][1]);
+        }
+        
+        //check for cycle
+        for(int i=0;i<numCourses;i++)
+        {
+            if(visited[i] == 0)
+            {
+                if(hasCycle(i, adjList, visited))
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    public bool hasCycle(int i, Dictionary<int, List<int>> adjList, int[] visited)
+    {
+        visited[i] = 1;
+        if(adjList.ContainsKey(i))
+        {
+            foreach(int neighbor in adjList[i])
+            {
+                if(visited[neighbor] == 0 && hasCycle(neighbor,adjList, visited) || visited[neighbor] == 1)
+                {
+                    return true;
+                }
+            }
+        }
+        visited[i] = 2;
+        return false;
+    }
+}
