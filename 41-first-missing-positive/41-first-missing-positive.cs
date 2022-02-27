b0f0476ed633b74054919baab9786ea6944aea9b
sreetirumannamalai <sree.tirumannamalai@gmail.com>
2022-02-26 21:20:30 -0800

    Time: 290 ms (72.27%), Space: 60 MB (98.28%) - LeetHub

---
+++ 41-first-missing-positive/41-first-missing-positive.cs
@@ -0,0 +1,40 @@
+public class Solution {
+    public int FirstMissingPositive(int[] nums) {
+        //O(n) sc O(1)
+        if(nums == null || nums.Length == 0)
+            return 1;
+        
+        int n = nums.Length;
+        int containsOne = 0;
+        
+        //step 1
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i] == 1)
+                containsOne = 1;
+            else if(nums[i] <= 0 || nums[i] > n)
+                nums[i] = 1;
+        }
+        
+        if(containsOne == 0) 
+            return 1;
+        
+        //step 2
+        for(int i =0;i< n;i++)
+        {
+            int index = Math.Abs(nums[i]) - 1;
+            if(nums[index] > 0)
+                nums[index] = -1 * nums[index];
+        }
+        
+        //step 3
+        for(int i=0;i< n;i++)
+        {
+            if(nums[i] > 0)
+                return (i + 1);
+        }
+        
+        //[1,2,3]-> 4
+        return n + 1;
+    }
+}
