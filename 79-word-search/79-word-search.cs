ad0d172b4c54aeb8870b7a0e1be74b3a954086fe
sreetirumannamalai <sree.tirumannamalai@gmail.com>
2022-02-28 23:33:38 -0800

    Time: 235 ms (94.72%), Space: 39 MB (80.46%) - LeetHub

--- 79-word-search/79-word-search.cs
+++ 79-word-search/79-word-search.cs
@@ -1,41 +1,40 @@
 public class Solution {
     //O(4 * n) = O(n), sc O(1)
-    public bool Exist(char[][] board, string word) {
-        int row = board.Length;
-        int col = board[0].Length;
-        for(int i=0;i<row;i++)
+     public bool Exist(char[][] board, string word)
         {
-            for(int j=0;j<col;j++)
+            int row = board.Length;
+            int col = board[0].Length;
+            for(int i=0;i<row;i++)
             {
-                if(board[i][j] == word[0] && Search(board, i, j, word, 0))
-                    return true;
+                for(int j=0;j<col;j++)
+                {
+                    if (board[i][j] == word[0] && WordSearch(board, i, j, word, 0))
+                        return true;
+                }
             }
+            return false;
         }
-        return false;
-    }
-    
-    public bool Search(char[][] board, int i, int j, string word, int index)
-    {
-        if(index == word.Length - 1)
+
+        public bool WordSearch(char[][] board, int i, int j, string word, int index)
         {
-            return true;
+            if (index == word.Length - 1)
+                return true;
+
+            board[i][j] -= (char)65; // O(1) operation // change to any other char outside of A-Z and a-z
+
+            if (i > 0 && board[i - 1][j] == word[index + 1] && WordSearch(board, i - 1, j, word, index + 1))
+                return true;
+
+            if (j > 0 && board[i][j - 1] == word[index + 1] && WordSearch(board, i, j - 1, word, index + 1))
+                return true;
+
+            if (i < board.Length - 1 && board[i + 1][j] == word[index + 1] && WordSearch(board, i + 1, j, word, index + 1))
+                return true;
+            if (j < board[0].Length - 1 && board[i][j + 1] == word[index + 1] && WordSearch(board, i, j + 1, word, index + 1))
+                return true;
+
+            board[i][j] += (char)65; //change the char back to original char to backtrack again
+
+            return false;
         }
-        
-        board[i][j] -= (char) 65; // change to any other character outside of A-Z and a-z range
-        if(i > 0 && board[i-1][j] == word[index+1] && Search(board, i - 1, j, word, index + 1 ))
-            return true;
-        
-         if(j > 0 && board[i][j-1] == word[index+1] && Search(board, i, j - 1, word, index + 1 ))
-            return true;
-        
-         if(i < board.Length - 1 && board[i+1][j] == word[index+1] && Search(board, i + 1, j, word, index + 1 ))
-            return true;
-        
-         if(j < board[0].Length - 1 && board[i][j+1] == word[index+1] && Search(board, i, j + 1, word, index + 1 ))
-            return true;
-        
-        
-        board[i][j] += (char) 65; // change the character back to original character to backtrack again
-        return false;
-    }
 }
