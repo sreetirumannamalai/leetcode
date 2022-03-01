3f1e6dd5977c68679ea0cb6928293d1be3079fad
sreetirumannamalai <sree.tirumannamalai@gmail.com>
2022-02-28 22:31:43 -0800

    Time: 200 ms (58.82%), Space: 44.6 MB (45.59%) - LeetHub

--- 57-insert-interval/57-insert-interval.cs
+++ 57-insert-interval/57-insert-interval.cs
@@ -3,28 +3,30 @@ public class Solution {
         if(intervals == null || intervals.Length == 0)
             return new int[][] {newInterval};
         
-        List<int[]> result = new List<int[]>();
-        int i = 0;
-        while(i < intervals.Length && intervals[i][1] < newInterval[0])
-        {
-            result.Add(intervals[i]);
-            i++;
-        }
-        
-        while(i < intervals.Length && intervals[i][0] <= newInterval[1])
-        {
-            newInterval[0] = Math.Min(intervals[i][0], newInterval[0]);
-            newInterval[1] = Math.Max(intervals[i][1], newInterval[1]);
-            i++;
-        }
-        result.Add(newInterval);
-        
-        while(i < intervals.Length)
-        {
-            result.Add(intervals[i]);
-            i++;
-        }
-        return result.ToArray();
+       List<int[]> result = new List<int[]>();
+            int i = 0;
+
+            // Step 1 - add all intervals ending before newInterval starts
+            while (i < intervals.Length && intervals[i][1] < newInterval[0])
+            {
+                result.Add(intervals[i]);
+                i++;
+            }
+
+            // Step 2 - update the newInterval by merging with all overlapping intervals
+            while (i < intervals.Length && intervals[i][0] <= newInterval[1])
+            {
+                newInterval[0] = Math.Min(newInterval[0], intervals[i][0]);
+                newInterval[1] = Math.Max(newInterval[1], intervals[i][1]);
+                i++;
+            }
+            result.Add(newInterval); // add updated interval
+
+            // Step 3 - add remaining intervals
+            while (i < intervals.Length)
+                result.Add(intervals[i++]);
+
+            return result.ToArray();
     }
 }
   
