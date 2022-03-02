220dd3fd27cdd8fc654f089e732428ce79338e86
sreetirumannamalai <sree.tirumannamalai@gmail.com>
2022-03-01 21:53:06 -0800

    Time: 119 ms (66.04%), Space: 39.6 MB (48.13%) - LeetHub

---
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.cs
@@ -0,0 +1,41 @@
+/*
+// Definition for a Node.
+public class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+}
+*/
+
+public class Solution {
+    //O(n) sc O(1)
+    public Node Connect(Node root) {
+     if(root == null || root.left == null || root.right == null)
+         return root;   
+        
+        root.left.next = root.right;
+        if(root.next!= null)
+        {
+            root.right.next = root.next.left;
+        }
+        
+        Connect(root.left);
+        Connect(root.right);
+        
+        return root;
+    }
+}
