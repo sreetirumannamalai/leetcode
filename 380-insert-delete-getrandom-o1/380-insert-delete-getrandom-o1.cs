b9da51a04dfe10bb12b7bb646bee0e6c9eb23b5e
sreetirumannamalai <sree.tirumannamalai@gmail.com>
2022-03-03 21:46:05 -0800

    Time: 615 ms (32.70%), Space: 87.7 MB (53.16%) - LeetHub

---
+++ 380-insert-delete-getrandom-o1/380-insert-delete-getrandom-o1.cs
@@ -0,0 +1,43 @@
+public class RandomizedSet {
+    HashSet<int> set;
+    List<int> list;
+    Random random;
+    public RandomizedSet() {
+        random = new Random();
+        set = new HashSet<int>();
+        list = new List<int>();
+    }
+    
+    public bool Insert(int val) {
+        if(set.Contains(val))
+            return false;
+        else 
+        {
+            set.Add(val);
+            list.Add(val);
+        }
+        return true;
+    }
+    
+    public bool Remove(int val) {
+        if(set.Contains(val))
+        {
+            set.Remove(val);
+            list.Remove(val);
+            return true;
+        }
+        return false;
+    }
+    
+    public int GetRandom() {
+        return list[random.Next(list.Count)];
+    }
+}
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * bool param_1 = obj.Insert(val);
+ * bool param_2 = obj.Remove(val);
+ * int param_3 = obj.GetRandom();
+ */
