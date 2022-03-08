611bb97523fff7f5f826cb10fc1c51e631b10c8a
sreetirumannamalai <sree.tirumannamalai@gmail.com>
2022-03-07 23:02:35 -0800

    Time: 93 ms (51.50%), Space: 35.6 MB (45.51%) - LeetHub

---
+++ 984-string-without-aaa-or-bbb/984-string-without-aaa-or-bbb.cs
@@ -0,0 +1,33 @@
+public class Solution {
+    //O(A + B) sc O(A+B)
+    public string StrWithout3a3b(int a, int b) {
+        StringBuilder sb = new StringBuilder();
+        while(a > 0 || b > 0)
+        {
+            bool writeA = false;
+            int L = sb.Length;
+            if(L >= 2 && sb[L-1] == sb[L-2])
+            {
+                if(sb[L-1] == 'b')
+                    writeA = true;
+            }
+            else
+            {
+                if(a >= b)
+                    writeA = true;
+            }
+            
+            if(writeA)
+            {
+                a--;
+                sb.Append('a');
+            }
+            else
+            {
+                b--;
+                sb.Append('b');
+            }
+        }
+        return sb.ToString();
+    }
+}
