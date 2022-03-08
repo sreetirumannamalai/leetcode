a312aed906a6b0e58420b18bcfb37dd74cb1198b
sreetirumannamalai <sree.tirumannamalai@gmail.com>
2022-03-07 21:26:48 -0800

    Time: 108 ms (85.25%), Space: 45.3 MB (5.76%) - LeetHub

---
+++ 285-inorder-successor-in-bst/285-inorder-successor-in-bst.cs
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    //O(n) sc O(1)
+    public TreeNode InorderSuccessor(TreeNode root, TreeNode p) {
+        TreeNode successor = null;
+        while(root != null)
+        {
+            if(p.val >= root.val)
+            {
+                root = root.right;
+            }
+            else
+            {
+                successor = root;
+                root = root.left;
+            }
+        }
+        return successor;
+    }
+}
