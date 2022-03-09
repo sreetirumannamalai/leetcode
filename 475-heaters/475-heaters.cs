e5bb44bd0b2799ee9a872f5977188ec1886e6a49
sreetirumannamalai <sree.tirumannamalai@gmail.com>
2022-03-08 20:36:12 -0800

    Time: 120 ms (90.70%), Space: 45.3 MB (6.98%) - LeetHub

---
+++ 475-heaters/475-heaters.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int FindRadius(int[] houses, int[] heaters) {
+        int n = houses.Length;
+        int m = heaters.Length;
+        Array.Sort(houses);
+        Array.Sort(heaters);
+        int j = 0,max = 0;
+        
+        for(int i=0;i<n;i++)
+        {
+            //move right until the next heater is closer or equal distance to current heater
+            while(j + 1 < m && Math.Abs(houses[i] - heaters[j+1]) <= Math.Abs(heaters[j] - houses[i]))
+            {
+                j++;
+            }
+            max = Math.Max(max, Math.Abs(heaters[j] - houses[i]));
+        }
+        return max;
+    }
+}
+
+ 
