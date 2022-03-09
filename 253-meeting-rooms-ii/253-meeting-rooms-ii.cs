a0116dd250aa9a88da7e6cda59f2053a3976dab5
sreetirumannamalai <sree.tirumannamalai@gmail.com>
2022-03-08 19:07:15 -0800

    Time: 98 ms (84.68%), Space: 39.5 MB (59.36%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.cs
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.cs
@@ -1,30 +1,31 @@
 public class Solution {
     public int MinMeetingRooms(int[][] intervals) {
         //O(nlogn) sc O(n)
-            if(intervals.Length == 0) return 0;
+        if(intervals.Length == 0) return 0;
         
-            int[] startTimes = new int[intervals.Length];
-            int[] endTimes = new int[intervals.Length];
-            
-            for(int i =0;i<intervals.Length;i++)
-            {
-                startTimes[i] = intervals[i][0];
-                endTimes[i] = intervals[i][1];
-            }
-            
-            Array.Sort(startTimes);
-            Array.Sort(endTimes);
-            
-            int startPtr = 0;
-            int endPtr = 0;
-            int count = 0;
-            for(int i = 0; i < startTimes.Length; i++)
-            {
-                if(startTimes[i] < endTimes[endPtr])
-                    startPtr++;
-                else
-                    endPtr++;
-            }
+        int[] startTimes = new int[intervals.Length];
+        int[] endTimes = new int[intervals.Length];
+        
+        for(int i=0;i<intervals.Length;i++)
+        {
+            startTimes[i] = intervals[i][0];
+            endTimes[i] = intervals[i][1];
+        }
+        
+        Array.Sort(startTimes);
+        Array.Sort(endTimes);
+        
+        int startPtr = 0;
+        int endPtr = 0;
+        int count = 0;
+        
+        for(int i=0;i<startTimes.Length;i++)
+        {
+            if(startTimes[i] < endTimes[endPtr])
+                startPtr++;
+            else
+                endPtr++;
+        }
         return startPtr;
     }
 }
