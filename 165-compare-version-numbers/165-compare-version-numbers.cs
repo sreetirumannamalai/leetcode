d6a03ce746b32e41b8762807eac7c4cea66dae32
sreetirumannamalai <sree.tirumannamalai@gmail.com>
2022-03-08 22:40:06 -0800

    Time: 110 ms (16.38%), Space: 35.3 MB (41.93%) - LeetHub

---
+++ 165-compare-version-numbers/165-compare-version-numbers.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int CompareVersion(string version1, string version2) {
+        string[] s1 = version1.Split(".");
+        string[] s2 = version2.Split(".");
+        
+        int maxLength = Math.Max(s1.Length, s2.Length);
+        for(int i=0;i<maxLength;i++)
+        {
+            int v1 = int.Parse(i<s1.Length ? s1[i] : "0");
+            int v2 = int.Parse(i<s2.Length ? s2[i] : "0");
+            
+            if(v1 < v2) return -1;
+            else if(v1 > v2) return 1;
+        }
+        return 0;
+    }
+}
