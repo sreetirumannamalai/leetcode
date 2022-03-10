093d6f961ff408cb36b785f8b2b754277a3a33ff
sreetirumannamalai <sree.tirumannamalai@gmail.com>
2022-03-09 19:33:54 -0800

    Time: 79 ms (80.06%), Space: 37.4 MB (89.43%) - LeetHub

--- 227-basic-calculator-ii/227-basic-calculator-ii.cs
+++ 227-basic-calculator-ii/227-basic-calculator-ii.cs
@@ -1,23 +1,22 @@
 public class Solution {
     public int Calculate(string s) {
         //O(n) sc O(1)
-        if(s == null || s.Length == 0) return 0;
+        if(s == null || s.Length == 0)
+            return 0;
         int length = s.Length;
         int currentNumber = 0;
         int lastNumber = 0;
         int result = 0;
         char operation = '+';
         
-        for(int i =0;i<length;i++)
+        for(int i=0;i<length;i++)
         {
             char currentChar = s[i];
             if(Char.IsDigit(currentChar))
             {
                 currentNumber = (currentNumber * 10) + (currentChar - '0');
             }
-            if(!Char.IsDigit(currentChar) && 
-               !Char.IsWhiteSpace(currentChar) ||
-               i == length - 1)
+            if(!Char.IsDigit(currentChar) && !Char.IsWhiteSpace(currentChar) || i == length - 1)
             {
                 if(operation == '+' || operation == '-')
                 {
