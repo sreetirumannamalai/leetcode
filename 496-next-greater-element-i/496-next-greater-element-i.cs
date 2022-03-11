ff50bc7bf6654e34488ea68f5e587e76ec2bdb0d
sreetirumannamalai <sree.tirumannamalai@gmail.com>
2022-03-10 19:29:12 -0800

    Time: 189 ms (57.25%), Space: 42 MB (48.96%) - LeetHub

---
+++ 496-next-greater-element-i/496-next-greater-element-i.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    //time O(N + M) where N and M are the lengths of nums1 and nums2
+    //space: O(M)
+    public int[] NextGreaterElement(int[] nums1, int[] nums2) {
+        
+        Dictionary<int,int> dic = new Dictionary<int,int>();
+        Stack<int> stack = new Stack<int>();
+        foreach(var num in nums2)
+        {
+            while(stack.Count > 0 && num > stack.Peek())
+                dic.Add(stack.Pop(),num);
+            
+            stack.Push(num);
+        }
+        
+        int[] res = new int[nums1.Length];
+        for(int i = 0; i < nums1.Length; i++)
+            res[i] = dic.ContainsKey(nums1[i])? dic[nums1[i]] : -1;
+        
+        return res;
+    }
+} 
