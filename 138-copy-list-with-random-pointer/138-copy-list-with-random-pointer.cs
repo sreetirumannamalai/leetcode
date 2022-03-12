76deba8bf62aa7167a71d4a689c2d9fe35f5040d
sreetirumannamalai <sree.tirumannamalai@gmail.com>
2022-03-12 15:29:50 -0800

    Time: 143 ms (35.31%), Space: 38.1 MB (33.60%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cs
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cs
@@ -16,8 +16,10 @@ public class Node {
 public class Solution {
     //O(n) sc O(1)
     public Node CopyRandomList(Node head) {
+        if(head == null) return null;
+        
         Node current = head;
-        //1. copy node and insert after actual nodes
+        //step 1 - copy nodes and insert after actual nodes
         while(current != null)
         {
             Node copy = new Node(current.val);
@@ -25,16 +27,16 @@ public class Solution {
             current.next = copy;
             current = current.next.next;
         }
-         
-        //2. map random pointer
+        
+        //step 2 - map random pointer
         current = head;
         while(current != null)
         {
-            current.next.random = current.random == null ? null : current.random.next;
+            current.next.random = (current.random == null) ? null : current.random.next;
             current = current.next.next;
         }
-         
-        //3. extract copy node
+        
+        //step 3 - extract copy node
         Node dummyhead = new Node(-1);
         Node dummy = dummyhead;
         current = head;
@@ -45,8 +47,6 @@ public class Solution {
             current.next = current.next.next;
             current = current.next;
         }
-         
         return dummyhead.next;
     }
 }
-  
