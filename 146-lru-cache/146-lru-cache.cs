e04a9b69d9cd5a4f5249fe31b4331186ae9d2716
sreetirumannamalai <sree.tirumannamalai@gmail.com>
2022-03-12 19:55:30 -0800

    Time: 1198 ms (35.87%), Space: 140.1 MB (42.39%) - LeetHub

--- 146-lru-cache/146-lru-cache.cs
+++ 146-lru-cache/146-lru-cache.cs
@@ -1,57 +1,50 @@
 public class LRUCache {
     public class Cache
     {
-        public int cacheKey;
-        public int cacheValue;
+        public int cachekey;
+        public int cachevalue;
         public Cache(int key, int value)
         {
-            this.cacheKey = key;
-            this.cacheValue = value;
+            this.cachekey = key;
+            this.cachevalue = value;
         }
     }
     
-    int capacity = 0;
-    Dictionary<int, LinkedListNode<Cache>> dict;
-    LinkedList<Cache> lruList;
-    
+    int capacity =0;
+    Dictionary<int, LinkedListNode<Cache>> dict = new Dictionary<int, LinkedListNode<Cache>>();
+    LinkedList<Cache> lruCache = new LinkedList<Cache>();
     public LRUCache(int capacity) {
-        dict = new Dictionary<int, LinkedListNode<Cache>>();
-        lruList = new LinkedList<Cache>();
         this.capacity = capacity;
     }
-     
+    
     public int Get(int key) {
         if(!dict.ContainsKey(key))
             return -1;
         
         LinkedListNode<Cache> cache = dict[key];
-        
-        lruList.Remove(cache);
-        lruList.AddFirst(cache);
-        return cache.Value.cacheValue;
+        lruCache.Remove(cache);
+        lruCache.AddFirst(cache);
+        return cache.Value.cachevalue;
     }
     
-   
-    public void Put(int key, int value)
-    {
+    public void Put(int key, int value) {
         if(dict.ContainsKey(key))
         {
-            dict[key].Value.cacheValue = value;
+            dict[key].Value.cachevalue = value;
             LinkedListNode<Cache> cache = dict[key];
-            lruList.Remove(cache);
-            lruList.AddFirst(cache);
+            lruCache.Remove(cache);
+            lruCache.AddFirst(cache);
         }
         else
         {
             Cache cache = new Cache(key, value);
             dict.Add(key, new LinkedListNode<Cache>(cache));
-            lruList.AddFirst(dict[key]);
-            
+            lruCache.AddFirst(dict[key]);
             if(dict.Count > capacity)
             {
-                LinkedListNode<Cache> lastNode = lruList.Last;
-                dict.Remove(lastNode.Value.cacheKey);
-                lruList.Remove(lastNode);
+                LinkedListNode<Cache> lastNode = lruCache.Last;
+                dict.Remove(lastNode.Value.cachekey);
+                lruCache.Remove(lastNode);
             }
         }
     }
