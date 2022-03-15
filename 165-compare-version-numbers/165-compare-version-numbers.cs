e51a263e473bce05560b18613c56791554ecec51
sreetirumannamalai <sree.tirumannamalai@gmail.com>
2022-03-14 20:41:01 -0700

    Time: 70 ms (77.22%), Space: 35.6 MB (23.25%) - LeetHub

--- 165-compare-version-numbers/165-compare-version-numbers.cs
+++ 165-compare-version-numbers/165-compare-version-numbers.cs
@@ -1,17 +1,19 @@
 public class Solution {
+    //O(n + m + Max(n,m)) sc O(n + m) m and n are length of input strings
     public int CompareVersion(string version1, string version2) {
         string[] s1 = version1.Split(".");
         string[] s2 = version2.Split(".");
-        
         int maxLength = Math.Max(s1.Length, s2.Length);
         for(int i=0;i<maxLength;i++)
         {
             int v1 = int.Parse(i<s1.Length ? s1[i] : "0");
             int v2 = int.Parse(i<s2.Length ? s2[i] : "0");
             
-            if(v1 < v2) return -1;
-            else if(v1 > v2) return 1;
+            if(v1 < v2) 
+                return -1;
+            else if(v1 > v2) 
+                return 1;
         }
         return 0;
     }
-}
+} 
