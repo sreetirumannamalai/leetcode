a19eba67b2e58f5b2ba60ee5b9acecc2d71c6e7d
sreetirumannamalai <sree.tirumannamalai@gmail.com>
2022-03-17 22:24:40 -0700

    Time: 1269 ms (21.50%), Space: 137 MB (87.60%) - LeetHub

--- 146-lru-cache/146-lru-cache.cs
+++ 146-lru-cache/146-lru-cache.cs
@@ -9,15 +9,17 @@ public class LRUCache {
             this.cachevalue = value;
         }
     }
-    
-    int capacity =0;
+    int capacity = 0;
     Dictionary<int, LinkedListNode<Cache>> dict = new Dictionary<int, LinkedListNode<Cache>>();
     LinkedList<Cache> lruCache = new LinkedList<Cache>();
-    public LRUCache(int capacity) {
+    
+    public LRUCache(int capacity)
+    {
         this.capacity = capacity;
     }
-    
-    public int Get(int key) {
+      
+    public int Get(int key)
+    {
         if(!dict.ContainsKey(key))
             return -1;
         
@@ -27,7 +29,8 @@ public class LRUCache {
         return cache.Value.cachevalue;
     }
     
-    public void Put(int key, int value) {
+    public void Put(int key, int value)
+    {
         if(dict.ContainsKey(key))
         {
             dict[key].Value.cachevalue = value;
