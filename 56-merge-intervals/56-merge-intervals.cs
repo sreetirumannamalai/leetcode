9f0a8fe8c76bde68f72527ff3061f448ad2538e3
sreetirumannamalai <sree.tirumannamalai@gmail.com>
2022-03-18 22:13:56 -0700

    Time: 168 ms (91.16%), Space: 48.6 MB (10.46%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cs
+++ 56-merge-intervals/56-merge-intervals.cs
@@ -1,15 +1,12 @@
 public class Solution {
     public int[][] Merge(int[][] intervals) {
-        List<int[]> result = new List<int[]>();
-        if(intervals.Length == 0)
-            return result.ToArray();
-        
+       List<int[]> result = new List<int[]>();
+        if(intervals.Length == 0) return result.ToArray();
         //Sort the array on start intervals
         Array.Sort(intervals, (x,y)=>x[0].CompareTo(y[0]));
-        
         int start = intervals[0][0];
         int end = intervals[0][1];
-        for(int i=1;i<intervals.Length;i++)
+        for(int i=0;i<intervals.Length;i++)
         {
             if(end < intervals[i][0])
             {
@@ -22,7 +19,7 @@ public class Solution {
                 end = Math.Max(end, intervals[i][1]);
             }
         }
-        result.Add(new int[]{start,end});
+        result.Add(new int[] {start, end});
         return result.ToArray();
     }
 }
