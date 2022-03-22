c908dc461657c256e1bfb3f1dcb0b03b3eaf56af
sreetirumannamalai <sree.tirumannamalai@gmail.com>
2022-03-21 19:02:25 -0700

    Time: 233 ms (18.02%), Space: 42.4 MB (67.49%) - LeetHub

--- 93-restore-ip-addresses/93-restore-ip-addresses.cs
+++ 93-restore-ip-addresses/93-restore-ip-addresses.cs
@@ -1,36 +1,44 @@
 public class Solution {
+     
     public IList<string> RestoreIpAddresses(string s) {
-       List<string> result = new List<string>();
+        List<string> result = new List<string>();
         
-       if(s.Length > 12)
-           return result;
+        if (s == null || s == string.Empty)
+            return result;
+        
+        if(s.Length > 12)
+            return result;
+         
         Helper(0, 0, s, new List<string>(), result);
         return result;
     }
     
-    
-    public void Helper(int index, int dots, string s, List<string> lst, List<string> result)
+    public void Helper(int index, int dots, string s, List<string> list, List<string> result)
     {
         if(dots == 4 && index == s.Length)
-            result.Add(string.Join('.', lst));
+        {
+            result.Add(string.Join('.',list));
+            Console.WriteLine(string.Join('.',list));
+        }
         
         if(dots == 4 && index != s.Length)
             return;
         
-        for(int i = 1 ; i < 4 && index + i <= s.Length; i++)
+        for(int i = 1; i < 4 && index + i <= s.Length; i++)
         {
-            if(IsValid(s, index , i))
+            if(IsValid(s, index, i))
             {
-                lst.Add(s.Substring(index, i));
-                Helper(index + i, dots + 1, s, lst, result);
-                lst.RemoveAt(lst.Count - 1);
+               list.Add(s.Substring(index, i));
+                Helper(index + i, dots + 1, s, list, result);
+                list.RemoveAt(list.Count - 1);
             }
         }
     }
-          
+   
     public bool IsValid(string s, int index, int len)
     {
         string sub = s.Substring(index, len);
+        
         if((sub.Length > 1 && sub[0] == '0') || Convert.ToInt32(sub) > 255)
         {
             return false;
@@ -38,4 +46,3 @@ public class Solution {
         return true;
     }
 }
-              
