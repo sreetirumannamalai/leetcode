f03a0b5f2243eec401f18c10ffec01eb5ab9d7cd
sreetirumannamalai <sree.tirumannamalai@gmail.com>
2022-03-22 21:25:16 -0700

    Time: 186 ms (5.20%), Space: 37.9 MB (49.75%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cs
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cs
@@ -1,7 +1,7 @@
 public class Solution {
     public int Search(int[] nums, int target) {
         //O(nlogn) sc O(1)
-        int start = 0;
+        int start =0;
         int end = nums.Length - 1;
         while(start <= end)
         {
@@ -11,24 +11,16 @@ public class Solution {
             else if(nums[mid] >= nums[start])
             {
                 if(target >= nums[start] && target <= nums[mid])
-                {
                     end = mid - 1;
-                }
                 else
-                {
                     start = mid + 1;
-                }
             }
             else
             {
                 if(target > nums[mid] && target <= nums[end])
-                {
                     start = mid + 1;
-                }
                 else
-                {
                     end = mid - 1;
-                }
             }
         }
         return -1;
