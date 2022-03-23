07a0665d9d919b7aacb33a3be8432adf45a66f3e
sreetirumannamalai <sree.tirumannamalai@gmail.com>
2022-03-22 19:58:01 -0700

    Time: 150 ms (79.20%), Space: 42.4 MB (46.69%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cs
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cs
@@ -1,32 +1,32 @@
 public class Solution {
     public IList<string> LetterCombinations(string digits) {
         List<string> result = new List<string>();
-        if(digits != null && digits.Length > 0)
-        {
-            string[] map = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-            LetterCombinations_DFS(digits, map, result, new StringBuilder(), 0);
-        }
+        
+        if(digits.Length ==0 || digits == null)
+            return result;
+        
+        string[] map = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        DFS(digits, map, result, new StringBuilder(), 0);
         return result;
     }
     
-    public void LetterCombinations_DFS(string digits, string[] map, List<string> result, StringBuilder sb, int index)
+    public void DFS(string digits, string[] map, List<string> result, StringBuilder sb, int index)
     {
-        //base case
         if(index == digits.Length)
         {
             result.Add(sb.ToString());
             return;
         }
         
-        //get each character from the string
+        //get each character from the string digits
         int number = int.Parse(digits[index].ToString());
-        //get the string for that number from the map
+        //get the corresponding string for number from the map
         string letterString = map[number];
-        for(int i=0;i<letterString.Length;i++)
+        for(int i =0;i<letterString.Length;i++)
         {
             char ch = letterString[i];
             sb.Append(ch);
-            LetterCombinations_DFS(digits, map, result, sb, index + 1);
+            DFS(digits, map, result, sb, index + 1);
             sb.Remove(sb.Length - 1, 1);
         }
     }
