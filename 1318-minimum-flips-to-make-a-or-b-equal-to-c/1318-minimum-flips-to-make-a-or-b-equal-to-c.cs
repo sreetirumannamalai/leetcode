39ab8999ba3383d36672dc1bdda983c36ddd8740
sreetirumannamalai <sree.tirumannamalai@gmail.com>
2022-03-24 00:22:13 -0700

    Time: 36 ms (57.14%), Space: 25.5 MB (35.71%) - LeetHub

--- 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cs
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.cs
@@ -1,20 +1,25 @@
 public class Solution {
+    //O(n)
    public int MinFlips(int a, int b, int c)
-        {
-             int count = 0;
-            while (a != 0 || b != 0 || c != 0)
-            {
-                if (((a & 1) | (b & 1)) != (c & 1))
-                {
-                    if (((a & 1) & (b & 1)) == 1)
-                        count++;
-                    count++;
-                }
-                a >>= 1;
-                b >>= 1;
-                c >>= 1;
-            }
-
-            return count;
-        }
+   {
+      int d = a | b;
+       int total = 0;
+       int x, y, z;
+       for(int i=0;i<32;i++)
+       {
+           x = a & (1 << i);
+           y = b & (1 << i);
+           z = c & (1 << i);
+           if(z == 0 && x != 0 && y == 0) //both a has !0 ith bit & b has ith bit 0 and c has 0 ith bit
+               total++;
+           else if(z == 0 && x == 0 && y != 0) // both a has 0 ith bit & b has ith bit !0 and c has 0 ith bit
+               total++;
+           else if(z == 0 && x != 0 && y != 0) // both a & b has ith bit !0 and c has 0 ith bit
+               total+=2;
+           else if(z!=0 && x ==0 && y == 0) //both a & b has ith bit 0 and c has !0 ith bit
+               total++;
+       }
+       return total;
+   }
 }
+    
