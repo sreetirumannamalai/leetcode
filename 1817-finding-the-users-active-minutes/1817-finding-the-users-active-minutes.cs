33df5b5ca20574ad650d003612a343298543d98c
sreetirumannamalai <sree.tirumannamalai@gmail.com>
2022-04-04 23:04:21 -0700

    Time: 525 ms (43.48%), Space: 53.1 MB (31.52%) - LeetHub

--- 1817-finding-the-users-active-minutes/1817-finding-the-users-active-minutes.cs
+++ 1817-finding-the-users-active-minutes/1817-finding-the-users-active-minutes.cs
@@ -1,28 +1,26 @@
 public class Solution {
     //O(n) sc O(n)
     public int[] FindingUsersActiveMinutes(int[][] logs, int k) {
-        
         int[] res = new int[k];
-        if(logs == null || logs.Length == 0)
+        if(logs.Length ==0 || logs == null)
             return res;
         
-        Dictionary<int, HashSet<int>> dic = new Dictionary<int, HashSet<int>>();
-        for(int i = 0; i < logs.Length; i++)
+        Dictionary<int, HashSet<int>> dict = new Dictionary<int, HashSet<int>>();
+        for(int i=0;i<logs.Length;i++)
         {
             int user = logs[i][0];
             int time = logs[i][1];
             
-            if(!dic.ContainsKey(user))
-                dic[user] = new HashSet<int>(){time};
+            if(!dict.ContainsKey(user))
+                dict[user] = new HashSet<int>(){time};
             else
-                dic[user].Add(time);
+                dict[user].Add(time);
         }
         
-        foreach(var kv in dic)
+        foreach(KeyValuePair<int, HashSet<int>> kv in dict)
         {
             res[kv.Value.Count - 1]++;
         }
-        
         return res;
     }
 }
