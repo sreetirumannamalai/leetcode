15190448736271ddd38629fb39225dd95968c40c
sreetirumannamalai <sree.tirumannamalai@gmail.com>
2022-04-13 22:14:40 -0700

    Time: 121 ms (83.74%), Space: 49.1 MB (5.28%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cs
+++ 200-number-of-islands/200-number-of-islands.cs
@@ -1,33 +1,36 @@
 public class Solution {
     public int NumIslands(char[][] grid) {
-        if(grid.Length == 0) return 0;
-        
-        int islandCount = 0;
+        if(grid.Length == 0)
+            return 0;
+        int numOfIslands = 0;
         int m = grid.Length;
         int n = grid[0].Length;
-        for(int i=0;i<m;i++)
+        for(int i =0;i<m;i++)
         {
             for(int j=0;j<n;j++)
             {
                 if(grid[i][j] == '1')
                 {
-                    dfs(i, j, m, n, grid);
-                    islandCount++;
+                    DFS(i, j, grid, m, n);
+                    numOfIslands++;
                 }
             }
         }
-        return islandCount;
+         
+        return numOfIslands;
     }
     
-    public void dfs(int i, int j, int m, int n, char[][] grid)
+    public void DFS(int i, int j, char[][] grid, int m, int n)
     {
         if(i < 0 || j < 0 || i >= m || j >= n || grid[i][j] == '0')
             return;
         
-        grid[i][j] = '0';
-        dfs(i+1,j, m,n,grid);
-        dfs(i-1,j, m,n,grid);
-        dfs(i,j+1, m,n,grid);
-        dfs(i,j-1, m,n,grid);
+         grid[i][j] = '0';
+        
+         DFS(i+1, j, grid, m, n);
+         DFS(i-1, j, grid, m, n);
+         DFS(i, j+1, grid, m, n);
+         DFS(i, j-1, grid, m, n);
     }
 }
+ 
