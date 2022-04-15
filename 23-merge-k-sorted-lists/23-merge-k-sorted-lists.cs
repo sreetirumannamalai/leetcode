630faa550a59f2bd588705aaf25292b1dd00c6e2
sreetirumannamalai <sree.tirumannamalai@gmail.com>
2022-04-14 21:20:17 -0700

    Time: 221 ms (36.27%), Space: 42.6 MB (21.33%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cs
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cs
@@ -18,32 +18,35 @@ public class Solution {
         
         ListNode head = Merge2Lists(lists[0],lists[1]);
         
-        for(int i=2; i < lists.Length ; i++)
+        for(int i = 2;i<lists.Length;i++)
         {
             head = Merge2Lists(head, lists[i]);
         }
+        
         return head;
     }
     
-    public ListNode Merge2Lists(ListNode l1, ListNode l2)
+    public ListNode Merge2Lists(ListNode list1, ListNode list2)
     {
         ListNode head = new ListNode();
         ListNode tail = head;
-        while(l1!=null && l2!= null)
+        
+        while(list1 != null && list2 != null)
         {
-            if(l1.val < l2.val)
+            if(list1.val < list2.val)
             {
-                tail.next = l1;
-                l1 = l1.next;
+                tail.next = list1;
+                list1 = list1.next;
             }
             else
             {
-                tail.next = l2;
-                l2 = l2.next;
+                tail.next = list2;
+                list2 = list2.next;
             }
             tail = tail.next;
         }
-        tail.next = (l1 == null) ? l2 : l1;
+        
+        tail.next = (list1 == null) ? list2 : list1;
         return head.next;
     }
 }
