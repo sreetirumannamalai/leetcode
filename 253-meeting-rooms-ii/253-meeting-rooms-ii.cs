0a58be90ea59f826b34aefeead2352747881c442
sreetirumannamalai <sree.tirumannamalai@gmail.com>
2022-04-14 21:03:35 -0700

    Time: 92 ms (92.70%), Space: 40.9 MB (21.04%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.cs
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.cs
@@ -1,15 +1,15 @@
 public class Solution {
     //O(nlogn) sc O(n)
     public int MinMeetingRooms(int[][] intervals) {
-        if(intervals.Length == 0) return -1;
+        if(intervals.Length == 0) return 0;
         
         int[] startTimes = new int[intervals.Length];
-        int[] endTimes = new int[intervals.Length];
+        int[] endTimes   = new int[intervals.Length];
         
         for(int i=0;i<intervals.Length;i++)
         {
             startTimes[i] = intervals[i][0];
-            endTimes[i] = intervals[i][1];
+            endTimes[i]   = intervals[i][1];
         }
         
         Array.Sort(startTimes);
