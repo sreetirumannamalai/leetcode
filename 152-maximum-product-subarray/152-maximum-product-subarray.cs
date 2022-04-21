02488cfe25a59139aa02ec23a5a1100bb152f9db
sreetirumannamalai <sree.tirumannamalai@gmail.com>
2022-04-20 22:14:49 -0700

    Time: 119 ms (48.94%), Space: 38.5 MB (49.13%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.cs
+++ 152-maximum-product-subarray/152-maximum-product-subarray.cs
@@ -1,24 +1,21 @@
 public class Solution {
     //O(n) sc O(1)
     public int MaxProduct(int[] nums) {
-       if(nums == null || nums.Length == 0)
-           return 0;
-        
-        int min = nums[0];
-        int max = nums[0];
-        int res = nums[0];
-        for(int i=1;i<nums.Length;i++)
-        {
-            int product1 = nums[i] * min;
-            int product2 = nums[i] * max;
-            int product3 = nums[i];
-            
-            res = Math.Max(res, Math.Max(Math.Max(product1, product2), product3));
-            
-            max = Math.Max(Math.Max(product1, product2), product3);
-            min = Math.Min(Math.Min(product1, product2), product3);
-        }
-        return res;
+      if (nums.Length == 0) return -1;
+            int currentMax = nums[0];
+            int currentMin = nums[0];
+            int finalMax = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                int temp = currentMax;
+                currentMax = Math.Max(Math.Max(currentMax * nums[i], currentMin * nums[i]), nums[i]);
+                currentMin = Math.Min(Math.Min(temp * nums[i], currentMin * nums[i]), nums[i]);
+                if (currentMax > finalMax)
+                {
+                    finalMax = currentMax;
+                }
+            }
+            return finalMax;
     }
 }
  
