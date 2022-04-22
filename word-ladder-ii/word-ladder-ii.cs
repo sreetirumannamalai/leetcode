df66576032889f6a3c1c61bf7e1bc05422837f9e
sreetirumannamalai <sree.tirumannamalai@gmail.com>
2022-04-21 22:42:46 -0700

    Time: 211 ms, Memory: 46.4 MB - LeetHub

---
+++ word-ladder-ii/word-ladder-ii.cs
@@ -0,0 +1,58 @@
+public class Solution {
+    public IList<IList<string>> FindLadders(string beginWord, string endWord, IList<string> wordList) {
+        
+        List<IList<string>> result = new List<IList<string>>();
+        if(beginWord.Length != endWord.Length)
+            return result;
+        
+        HashSet<string> set = new HashSet<string>(wordList);
+        List<string> path = new List<string>(){beginWord};
+        
+        Queue<List<string>> queue = new Queue<List<string>>();
+        queue.Enqueue(path);
+        
+        HashSet<string> visited = new HashSet<string>();
+        bool found = false;
+        
+        while(queue.Count > 0)
+        {
+            int size = queue.Count;
+            for(int i=0;i<size;i++)
+            {
+                List<string> currList = queue.Dequeue();
+                string currString = currList[currList.Count - 1];
+                char[] arr = currString.ToCharArray();
+                for(int j=0;j<arr.Length;j++)
+                {
+                    char originalChar = arr[j];
+                    for(char c='a';c<='z';c++)
+                    {
+                        arr[j] = c;
+                        string next = new string(arr);
+                        if(set.Contains(next))
+                        {
+                            visited.Add(next);
+                            currList.Add(next);
+                            if(next == endWord)
+                            {
+                                found = true;
+                                result.Add(new List<string>(currList));
+                            }
+                            queue.Enqueue(new List<string>(currList));
+                            currList.RemoveAt(currList.Count -1);
+                        }
+                    }
+                    arr[j] = originalChar;
+                }
+            }
+            foreach(string str in visited)
+            {
+                set.Remove(str);
+            }
+            
+            if(found)
+                break;
+        }
+        return result;
+    }
+}
