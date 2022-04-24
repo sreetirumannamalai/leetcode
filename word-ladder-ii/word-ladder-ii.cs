51171934516aeb318fdb3d7baad8cd799664c340
sreetirumannamalai <sree.tirumannamalai@gmail.com>
2022-04-23 22:27:12 -0700

    Time: 262 ms, Memory: 46.5 MB - LeetHub

--- word-ladder-ii/word-ladder-ii.cs
+++ word-ladder-ii/word-ladder-ii.cs
@@ -1,16 +1,13 @@
 public class Solution {
     public IList<IList<string>> FindLadders(string beginWord, string endWord, IList<string> wordList) {
-        
         List<IList<string>> result = new List<IList<string>>();
-        if(beginWord.Length != endWord.Length)
-            return result;
+        if(beginWord.Length != endWord.Length) return result;
         
-        HashSet<string> set = new HashSet<string>(wordList);
-        List<string> path = new List<string>(){beginWord};
+        HashSet<string> wordSet = new HashSet<string>(wordList);
+        List<string> path = new List<string>() { beginWord};
         
         Queue<List<string>> queue = new Queue<List<string>>();
         queue.Enqueue(path);
-        
         HashSet<string> visited = new HashSet<string>();
         bool found = false;
         
@@ -20,26 +17,26 @@ public class Solution {
             for(int i=0;i<size;i++)
             {
                 List<string> currList = queue.Dequeue();
-                string currString = currList[currList.Count - 1];
-                char[] arr = currString.ToCharArray();
+                string current = currList[currList.Count - 1];
+                char[] arr = current.ToCharArray();
                 for(int j=0;j<arr.Length;j++)
                 {
                     char originalChar = arr[j];
-                    for(char c='a';c<='z';c++)
+                    for(char c='a'; c<= 'z';c++)
                     {
-                        arr[j] = c;
+                        arr[j]=c;
                         string next = new string(arr);
-                        if(set.Contains(next))
+                        if(wordSet.Contains(next))
                         {
                             visited.Add(next);
                             currList.Add(next);
                             if(next == endWord)
                             {
                                 found = true;
-                                result.Add(new List<string>(currList));
+                                result.Add(new List<string> (currList));
                             }
                             queue.Enqueue(new List<string>(currList));
-                            currList.RemoveAt(currList.Count -1);
+                            currList.RemoveAt(currList.Count - 1);
                         }
                     }
                     arr[j] = originalChar;
@@ -47,7 +44,7 @@ public class Solution {
             }
             foreach(string str in visited)
             {
-                set.Remove(str);
+                wordSet.Remove(str);
             }
             
             if(found)
