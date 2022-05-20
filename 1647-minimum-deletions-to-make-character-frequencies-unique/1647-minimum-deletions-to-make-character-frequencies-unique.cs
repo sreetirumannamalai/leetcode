846cbb0414302935dbfbca942f6d9a98838e38e2
sreetirumannamalai <sree.tirumannamalai@gmail.com>
2022-05-19 19:38:47 -0700

    Time: 193 ms (15.90%), Space: 40.6 MB (93.25%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cs
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cs
@@ -1,34 +1,32 @@
 public class Solution {
     public int MinDeletions(string s) {
+        if(s.Length == 0 || s == null) return 0;
         
-        if(s == null || s.Length == 0)
-            return 0;
-        
-        // record unique frquencies 
-        HashSet<int> freqSet = new HashSet<int>();
-        int res = 0;
-        
-        // calculate frequencies
-        int[] freq = new int[26];
-        foreach(var c in s)
-            freq[c - 'a']++;
-        
-        for(int i = 0; i < freq.Length; i++)
+        Dictionary<char,int> dict = new Dictionary<char, int>();
+        foreach(char c in s)
         {
-            // if there is another character has the same frequence
-            // we need to keep delete the current character until its frequncey is unique
-            // example 1: input is {(a,3), {b,3}, {c,2}}
-            // then we need to delete 2 a or 2 b
-            // example 2: input is {(a,3), {b,3}, {c,2},{d,2}}
-            // then we need to delete 2 a or 2 b, then delete 2 c or 2d
-            while(freqSet.Contains(freq[i]) && freq[i] > 0)
+            if(!dict.ContainsKey(c))
             {
-                freq[i]--;
-                res++;
+                dict.Add(c,1);
             }
-            freqSet.Add(freq[i]);
+            else
+            {
+                dict[c]++;
+            }
         }
         
-        return res;
+        int result = 0;
+        HashSet<int> set = new HashSet<int>();
+        foreach(var kv in dict)
+        {
+            int val = dict[kv.Key];
+            while(set.Contains(val) && val > 0)
+            {
+                val--;
+                result++;
+            }
+            set.Add(val);
+        }
+        return result;
     }
 }
