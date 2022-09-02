0cb1d5e93b9a5c6bc8c6f79cf0aea52c5875ffa4
sreetirumannamalai <sree.tirumannamalai@gmail.com>
2022-09-01 21:16:38 -0700

    Time: 134 ms (56.11%), Space: 41.6 MB (70.85%) - LeetHub

---
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cs
@@ -0,0 +1,28 @@
+public class Solution {
+    public bool ValidPalindrome(string s) {
+        int i = 0;
+        int j = s.Length-1;
+        while(i < j)
+        {
+            if(s[i] != s[j])
+                return IsPalindrome(s, i, j-1) || IsPalindrome(s, i+1, j);
+        
+            i++;
+            j--;
+        }
+        return true;
+    }
+    
+      public bool IsPalindrome(string s, int left, int right)
+      {
+        while(left < right)
+        {
+            if(s[left] != s[right])
+                return false;
+        
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
