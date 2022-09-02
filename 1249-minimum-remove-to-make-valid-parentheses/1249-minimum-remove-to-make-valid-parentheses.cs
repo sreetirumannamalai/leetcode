15bec43149343cafc0109c79074e778b095b0bb0
sreetirumannamalai <sree.tirumannamalai@gmail.com>
2022-09-02 14:45:34 -0700

    Time: 122 ms (66.10%), Space: 41 MB (72.60%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cs
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cs
@@ -1,9 +1,11 @@
 public class Solution {
     //TC O(n) SC O(n)
     public string MinRemoveToMakeValid(string s) {
-        if(s.Length == 0 || s == null) return null;
-        int open =0;
+         if(s==null || s.Length == 0) return null;
+        
         StringBuilder sb = new StringBuilder();
+        
+        int open = 0;
         foreach(char c in s.ToCharArray())
         {
             if(c == '(')
@@ -11,18 +13,17 @@ public class Solution {
             else if(c == ')')
             {
                 if(open == 0) continue;
-                
                 open--;
             }
             sb.Append(c);
         }
         
         StringBuilder result = new StringBuilder();
-        for(int i=sb.Length -1; i>=0;i--)
+        for(int i= sb.Length -1;i>=0;i--)
         {
-            if(sb[i] == '(' && open-- > 0)
+            if(sb[i]=='(' && open-- > 0)
                 continue;
-                
+            
             result.Append(sb[i]);
         }
         
