fe9ca818801690bdb1b6e20f5a29ccedf16833e4
sreetirumannamalai <sree.tirumannamalai@gmail.com>
2022-09-07 22:59:07 -0700

    Time: 388 ms (12.92%), Space: 46.9 MB (28.68%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cs
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cs
@@ -3,42 +3,41 @@ public class Solution {
       //TC O(n)in average case O(n2) in worst case SC O(1)
       public int FindKthLargest(int[] arr, int k)
       {
-          int low = 0;
+           int low = 0;
           int high = arr.Length - 1;
           int n = arr.Length;
-          int pivot = FindKthLargest(arr, n - k, low, high);
+          int pivot = FindKthLargest(arr, n-k, low, high);
           return pivot;
       }
-
-      public int FindKthLargest(int[] arr, int k, int low, int high)
-      {
-          int pivotPoint = GetPivotPoint(arr, low, high);
-          if(pivotPoint == k)
-              return arr[pivotPoint];
-          else if(pivotPoint < k)
-              return FindKthLargest(arr, k, pivotPoint + 1, high);
-          else 
-              return FindKthLargest(arr, k, low, pivotPoint - 1 );
-      }
     
-     private int GetPivotPoint(int[] arr, int low, int high)
-     {
-         int pivotElement = arr[high];
-         int pivotPoint = low;
-         for(int i= low;i<=high;i++)
-         {
-             if(arr[i]<pivotElement)
-             {
+    public int FindKthLargest(int[] arr, int k, int low, int high)
+    {
+        int pivotPoint = GetPivotPoint(arr, low, high);
+        if(pivotPoint == k)
+            return arr[pivotPoint];
+        else if(pivotPoint < k)
+            return FindKthLargest(arr, k, pivotPoint + 1, high);
+        else
+            return FindKthLargest(arr, k, low, pivotPoint - 1);
+    }
+    
+    public int GetPivotPoint(int[] arr, int low, int high)
+    {
+        int pivotElement = arr[high];
+        int pivotPoint = low;
+        for(int i= low;i<=high;i++)
+        {
+            if(arr[i] < pivotElement)
+            {
                  int temp = arr[i];
                  arr[i] = arr[pivotPoint];
                  arr[pivotPoint] = temp;
                  pivotPoint++;
-             }
-         }
-         int tmp = arr[pivotPoint];
-         arr[pivotPoint] = arr[high];
-         arr[high] = tmp;
-         
-         return pivotPoint;
-     }
+            }
+        }
+        int tmp = arr[pivotPoint];
+        arr[pivotPoint] = arr[high];
+        arr[high] = tmp;
+        return pivotPoint;
+    }
 }
