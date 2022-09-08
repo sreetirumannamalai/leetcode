012b2c6ce559a088ab29c964b3a5791eb205d287
sreetirumannamalai <sree.tirumannamalai@gmail.com>
2022-09-07 23:35:13 -0700

    Time: 168 ms (5.06%), Space: 37.3 MB (75.95%) - LeetHub

---
+++ 71-simplify-path/71-simplify-path.cs
@@ -0,0 +1,46 @@
+public class Solution {
+    //Simplify Path
+    //TC O(n) SC O(n);
+   public string SimplifyPath(string path) {
+        string[] components = path.Split('/');
+        Stack<string> stk = new Stack<string>();
+        
+        for(int i=0;i<components.Length;i++)
+        {
+            switch(components[i])
+            {
+                    case ".":
+                    case "":
+                        break;
+                    
+                    case "..":
+                    if(stk.Count>0)
+                    {
+                        stk.Pop();
+                    }
+                    break;
+                    
+                default:
+                    stk.Push(components[i]);
+                    break;
+            }
+        }
+        
+        StringBuilder res = new StringBuilder();
+        
+        List<string> list = new List<string>();
+        while(stk.Count>0)
+        {
+            list.Add(stk.Pop());
+        }
+        
+       
+        for(int i=list.Count-1;i>-1;i--)
+        {
+            res.Append("/");
+            res.Append(list[i]);
+        }
+        
+        return res.Length>0 ? res.ToString() : "/" ;               
+    }
+}
