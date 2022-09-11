60a250ae15f8ba59edfad2bddddd51a6f0660fc1
sreetirumannamalai <sree.tirumannamalai@gmail.com>
2022-09-11 11:33:34 -0700

    Time: 585 ms (18.49%), Space: 55.1 MB (76.71%) - LeetHub

--- 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.cs
+++ 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.cs
@@ -4,7 +4,7 @@ public class Solution {
     public int[] FindBuildings(int[] heights) {
         List<int> list =new List<int>(){heights.Length - 1};
         int max = heights[heights.Length - 1];
-        for(int i=heights.Length-2;i >-1;i--)
+        for(int i=heights.Length-2;i >=0;i--)
         {
             if(heights[i] > max)
             {
