575ec62b18e05d472f9131f0f8799341f70ed6fc
sreetirumannamalai <sree.tirumannamalai@gmail.com>
2022-09-10 20:53:01 -0700

    Time: 170 ms (5.52%), Space: 37.1 MB (91.41%) - LeetHub

--- 71-simplify-path/71-simplify-path.cs
+++ 71-simplify-path/71-simplify-path.cs
@@ -2,45 +2,41 @@ public class Solution {
     //Simplify Path
     //TC O(n) SC O(n);
    public string SimplifyPath(string path) {
-        string[] components = path.Split('/');
-        Stack<string> stk = new Stack<string>();
-        
-        for(int i=0;i<components.Length;i++)
-        {
-            switch(components[i])
-            {
-                    case ".":
-                    case "":
-                        break;
-                    
-                    case "..":
-                    if(stk.Count>0)
-                    {
-                        stk.Pop();
-                    }
-                    break;
-                    
-                default:
-                    stk.Push(components[i]);
-                    break;
-            }
-        }
-        
-        StringBuilder res = new StringBuilder();
-        
-        List<string> list = new List<string>();
-        while(stk.Count>0)
-        {
-            list.Add(stk.Pop());
-        }
-        
+       string[] components = path.Split('/');
+       Stack<string> stk = new Stack<string>();
+       for(int i =0;i<components.Length;i++)
+       {
+           switch(components[i])
+           {
+               case ".":
+               case "" :
+                  break;
+               case "..":
+               if(stk.Count > 0)
+               {
+                   stk.Pop();
+               }
+               break;
+               default:
+                   stk.Push(components[i]);
+                   break;
+           }
+       }
+         
+       StringBuilder res = new StringBuilder();
+       List<string> list = new List<string>();
        
-        for(int i=list.Count-1;i>-1;i--)
-        {
+       while(stk.Count > 0)
+       {
+            list.Add(stk.Pop());
+       }
+         
+       for(int i=list.Count - 1; i >= 0; i--)
+       {
             res.Append("/");
             res.Append(list[i]);
-        }
-        
-        return res.Length>0 ? res.ToString() : "/" ;               
+       }
+       
+        return res.Length > 0 ? res.ToString() : "/"; 
     }
 }
