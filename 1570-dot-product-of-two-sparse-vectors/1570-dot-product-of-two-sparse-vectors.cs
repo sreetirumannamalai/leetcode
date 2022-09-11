cf0327763b3dfb3de4141a03657ce14be84e23a1
sreetirumannamalai <sree.tirumannamalai@gmail.com>
2022-09-11 11:25:22 -0700

    Time: 836 ms (5.40%), Space: 71 MB (5.85%) - LeetHub

--- 1570-dot-product-of-two-sparse-vectors/1570-dot-product-of-two-sparse-vectors.cs
+++ 1570-dot-product-of-two-sparse-vectors/1570-dot-product-of-two-sparse-vectors.cs
@@ -3,12 +3,11 @@ public class SparseVector {
    //Let nn be the length of the input array and LL be the number of non-zero elements.
    //Time complexity: O(n) for creating the Hash Map; O(L) for calculating the dot product.
    //Space complexity: O(L) for creating the Hash Map, as we only store elements that are non-zero. O(1) for calculating the dot product.
-    Dictionary<int ,int> indexAndValueDict = new Dictionary<int, int>();
+    Dictionary<int, int> indexAndValueDict = new Dictionary<int, int>();
     public SparseVector(int[] nums) {
         for(int i=0;i<nums.Length;i++)
         {
-            if(nums[i]!=0)
-                indexAndValueDict.Add(i, nums[i]);
+            indexAndValueDict.Add(i, nums[i]);
         }
     }
     
