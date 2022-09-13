286487bb5f80e136af8caeb9468f0c82fd9b3a7b
sreetirumannamalai <sree.tirumannamalai@gmail.com>
2022-09-13 13:24:01 -0700

    Time: 121 ms (54.33%), Space: 38.4 MB (90.41%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public bool IsPalindrome(string s) {
+        if(s.Length == 0) return true;
+        if(s == null) return false;
+        
+        int low = 0;
+        int high = s.Length -1;
+        while(low < high)
+        {
+            while(low < high && !Char.IsLetterOrDigit(s[low]))
+            {
+                low++;
+            }
+            while(low < high && !Char.IsLetterOrDigit(s[high]))
+            {
+                high--;
+            }
+            
+            if(low < high && Char.ToLower(s[low++]) != Char.ToLower(s[high--]))
+                return false;
+        }
+        return true;
+    }
+}  
+ 
