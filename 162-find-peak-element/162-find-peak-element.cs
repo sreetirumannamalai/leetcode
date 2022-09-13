e8586ba7c76427f3d8a09927be8bab1091e74ab2
sreetirumannamalai <sree.tirumannamalai@gmail.com>
2022-09-13 13:11:34 -0700

    Time: 143 ms (31.32%), Space: 37.5 MB (73.25%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.cs
+++ 162-find-peak-element/162-find-peak-element.cs
@@ -1,18 +1,22 @@
 public class Solution {
     //Find Peak Element
-    //Time complexity :O(log2(n)). We reduce the search space in half at every step. Thus, the total search space will be consumed in log2(n) steps. Here, nn refers to the size of numsnums array.
+    //Time complexity :O(log2(n)). We reduce the search space in half at every step. Thus, the total search space will be consumed in log2(n) steps. Here, n refers to the size of nums array.
     //Space complexity : O(1). Constant extra space is used. 
     public int FindPeakElement(int[] nums) {
-        int l = 0;
-        int r = nums.Length - 1;
-        while(l < r)
-        {
-            int mid = (l + r)/2;
-            if(nums[mid] > nums[mid + 1])
-                r = mid;
-            else
-                l = mid + 1;
-        }
-        return l;
+       int low = 0;
+       int high = nums.Length - 1;
+      while(low < high)
+      {
+          int mid = (low + high)/2;
+          if(nums[mid]>nums[mid+1])
+          {
+              high = mid;
+          }
+          else
+          {
+              low = mid + 1;
+          }
+      }
+        return low;
     }
 }
