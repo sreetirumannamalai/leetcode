2091ed8bad91ed110d154ec8a78f5852a9a87d54
sreetirumannamalai <sree.tirumannamalai@gmail.com>
2022-09-12 19:09:15 -0700

    Time: 251 ms (37.82%), Space: 40.7 MB (81.45%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cs
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cs
@@ -2,16 +2,19 @@ public class Solution {
     //Shortest Path in Binary Matrix
     //TC O(n) SC O(n)
     public int ShortestPathBinaryMatrix(int[][] grid) {
+        if(grid.Length == 0) return -1;
+        
         int row = grid.Length;
         int col = grid[0].Length;
-        //corner case
+        
         if(grid[0][0] == 1 || grid[row-1][col-1] == 1)
             return -1;
-        
-        Queue<int[]> queue = new Queue<int[]>();
-        queue.Enqueue(new int[]{0,0});
-        bool[,] visited = new bool[row, col];
+
+        Queue<int[]> q = new Queue<int[]>();
+        q.Enqueue(new int[]{0,0});
+        bool[,] visited = new bool[row,col];
         visited[0,0] = true;
+        int level = 1;
         
         int[][] directions = {
             new int[] {1,0},
@@ -23,32 +26,30 @@ public class Solution {
             new int[] {0,1},
             new int[] {0,-1}
         };
-        
-        int level = 1;
-        while(queue.Count !=0)
+        while(q.Count != 0)
         {
-            int size = queue.Count;
-            for(int i =0;i<size;i++)
+            int size = q.Count;
+            for(int i= 0;i<size;i++)
             {
-                int[] node = queue.Dequeue();
-                if(node[0] == row - 1 && node[1] == col - 1)
-                    return level;
-                
-                for(int j = 0; j < directions.Length; j++)
+                int[] node = q.Dequeue();
+                if(node[0] == row -1 && node[1] == col -1)
                 {
-                    int neighbor_x = directions[j][0] +  node[0];
+                    return level;
+                }
+                
+                for(int j =0;j<directions.Length ;j++)
+                {
+                    int neighbor_x = directions[j][0] + node[0];
                     int neighbor_y = directions[j][1] + node[1];
                     
                     if(neighbor_x >= 0 && neighbor_x < row &&
                        neighbor_y >= 0 && neighbor_y < col &&
-                       !visited[neighbor_x, neighbor_y] &&
-                       grid[neighbor_x][neighbor_y] == 0)
+                       grid[neighbor_x][neighbor_y] == 0 && !visited[neighbor_x , neighbor_y])
                     {
-                        queue.Enqueue(new int[]{neighbor_x, neighbor_y});
+                        q.Enqueue(new int[]{neighbor_x, neighbor_y});
                         visited[neighbor_x, neighbor_y] = true;
                     }
                 }
-                
             }
             level++;
         }
